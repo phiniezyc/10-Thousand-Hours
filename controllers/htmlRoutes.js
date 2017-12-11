4ccd119f8d816381da7570cc966d4e5347d68bab
Chance Phiniezy <phiniezyc@gmail.com>
2017-12-11 02:37:46 -0500

    added index page and a addTask page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -2,12 +2,20 @@ const path = require("path");
 
 
 module.exports = (app) => {
-    app.get("/", (req, res) =>{
+    app.get("/", (req, res) => {
 
         res.render("index", {
-        
+
         });
 
+    });
+
+    app.get("/addTask", (req, res) => {
+        
+
+        res.render("addTask", {
+            
+                    });
     });
 
 };
